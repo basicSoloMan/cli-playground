df03a8e5860ca5afcee2093111320bccb228cd8f
Marius <mariusjansevanrensburg1@gmail.com>
2025-03-14 06:46:57 +0200

    feat: termbox implementation
    Implement termox to help with creating a basic text editor

--- go.mod
+++ go.mod
@@ -1,1 +1,9 @@
 module cli-playground
+
+go 1.23.4
+
+require (
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+)
--- main.go
+++ main.go
@@ -1,24 +1,37 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"github.com/mattn/go-runewidth"
+	"github.com/nsf/termbox-go"
 	"os"
-	"strings"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Simple Shell")
-	fmt.Println("------------")
+	runEditor()
+}
 
-	lineNumber := 1
+func runEditor() {
+	if err := termbox.Init(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer termbox.Close()
 
-	for {
-		fmt.Printf("%d ", lineNumber)
-		text, _ := reader.ReadString('\n')
+	printMessage(25, 11, termbox.ColorDefault, termbox.ColorDefault, "EGO Text Editor")
 
-		text = strings.Replace(text, "\r\n", "", -1)
-		lineNumber++
+	err := termbox.Flush()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	termbox.PollEvent()
+}
+
+func printMessage(col, row int, fg, bg termbox.Attribute, message string) {
+	for _, ch := range message {
+		termbox.SetCell(col, row, ch, fg, bg)
+		col += runewidth.RuneWidth(ch)
 	}
 }
