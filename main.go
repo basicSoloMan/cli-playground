168d4b5f86c3fbb95d7641323fbea7289d1242b8
Marius <mariusjansevanrensburg1@gmail.com>
2025-03-19 07:03:44 +0200

    feat: Add display buffer function

--- main.go
+++ main.go
@@ -7,6 +7,14 @@ import (
 	"os"
 )
 
+var ROWS, COLS int
+var offsetX, offsetY int
+
+var textBuffer = [][]rune{
+	{'h', 'e', 'l', 'l', 'o'},
+	{'w', 'o', 'r', 'l', 'd'},
+}
+
 func main() {
 	runEditor()
 }
@@ -18,9 +26,21 @@ func runEditor() {
 	}
 
 	for {
-		printMessage(25, 11, termbox.ColorDefault, termbox.ColorDefault, "EGO Text Editor")
+		COLS, ROWS = termbox.Size()
+		ROWS--
 
-		err := termbox.Flush()
+		if COLS < 80 {
+			COLS = 80
+		}
+
+		err := termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+		if err != nil {
+			return
+		}
+
+		displayTextBuffer()
+
+		err = termbox.Flush()
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
@@ -39,5 +59,27 @@ func printMessage(col, row int, fg, bg termbox.Attribute, message string) {
 	for _, ch := range message {
 		termbox.SetCell(col, row, ch, fg, bg)
 		col += runewidth.RuneWidth(ch)
+	}
+}
+
+func displayTextBuffer() {
+	var row, col int
+
+	for row = 0; row < ROWS; row++ {
+		textBufferRow := row + offsetY
+		for col = 0; col < COLS; col++ {
+			textBufferCol := col + offsetX
+			if textBufferRow >= 0 && textBufferRow < len(textBuffer) && textBufferCol < len(textBuffer[textBufferRow]) {
+				if textBuffer[textBufferRow][textBufferCol] != '\t' {
+					termbox.SetChar(col, row, textBuffer[textBufferRow][textBufferCol])
+				} else {
+					termbox.SetCell(col, row, rune(' '), termbox.ColorDefault, termbox.ColorGreen)
+				}
+			} else if row+offsetY > len(textBuffer)-1 {
+				termbox.SetCell(0, row, rune('*'), termbox.ColorBlue, termbox.ColorDefault)
+			}
+		}
+
+		termbox.SetChar(col, row, rune('\n'))
 	}
 }
